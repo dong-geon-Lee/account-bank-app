b18713f2e0b1a5f7337835a94e6fbdf6a3999691
이동건 <developer@idong-geon-ui-MacBookPro.local>
2022-12-14 10:31:47 +0900

    테스트 완료

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import GlobalStyle from "./styles/GlobalStyle";
 import Account from "./pages/Account";
 
 const App = () => {
-  console.log("여기는 recoil 브랜치입니다");
   return (
     <>
       <GlobalStyle />
